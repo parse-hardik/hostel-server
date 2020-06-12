697fcd8248c897621f5e52cc1cd9c64dd27d035c
parse-hardik <f20180062@hyderabad.bits-pilani.ac.in>
2020-06-12 19:38:49 +0530

    Multiple Notifications Disallow

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-var {Users} = require('./models/Users');
-var {GroupList} = require('./models/GroupList');
-var {Notification} = require('./models/Notifications');
-var {Wing}=require('./models/Wing');
-var {Timer}=require('./models/Timer');
+var { Users } = require('./models/Users');
+var { GroupList } = require('./models/GroupList');
+var { Notification } = require('./models/Notifications');
+var { Wing } = require('./models/Wing');
+var { Timer } = require('./models/Timer');
 var connection = require('./config.js').mongooseConnection;
 
 var app = express();
-app.use(cors({origin: true, credentials: true}));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors({ origin: true, credentials: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose.set('useCreateIndex', true);
@@ -19,82 +19,81 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
 DATABASE_CONNECTION = connection;
 
-mongoose.connect(DATABASE_CONNECTION,{useNewUrlParser: true});
+mongoose.connect(DATABASE_CONNECTION, { useNewUrlParser: true });
 
-app.get("/",(req,res)=>{
-	var ty = {userf:req.body.username};
+app.get("/", (req, res) => {
+	var ty = { userf: req.body.username };
 	res.send(ty);
 });
 
-app.get("/getGroup",(req,res)=>{
-	GroupList.find(function(err,finalgrps) {
-		if(err){
+app.get("/getGroup", (req, res) => {
+	GroupList.find(function (err, finalgrps) {
+		if (err) {
 			console.log(err);
 		}
-		else{
+		else {
 			res.json(finalgrps);
 			// console.log(finalgrps);
 		}
 
 	})
 });
-app.get("/getUsers",(req,res)=>{
-	Users.find(function(err,allUsers) {
-		if(err){
+app.get("/getUsers", (req, res) => {
+	Users.find(function (err, allUsers) {
+		if (err) {
 			console.log(err);
 		}
-		else{
+		else {
 			res.json(allUsers);
 		}
 
 	})
 });
 
-app.get("/getWing",(req,res)=>{
-	Wing.find(function(err,allWings){
-		if(err)
-		{
+app.get("/getWing", (req, res) => {
+	Wing.find(function (err, allWings) {
+		if (err) {
 			console.log(err);
 			res.json(err);
 		}
-		else{
+		else {
 			res.json(allWings);
 			console.log(allWings);
 		}
 	})
 });
 
-app.post("/signIn",(req,res)=>{
-	var {username,password} = req.body;
+app.post("/signIn", (req, res) => {
+	var { username, password } = req.body;
 	console.log(username);
-	Users.findOne({username:username,password:password},(err,result)=>{
-		if(err)
+	Users.findOne({ username: username, password: password }, (err, result) => {
+		if (err)
 			res.send(err);
-		else{
+		else {
 			res.send(result);
 			console.log(result);
 		}
 	});
 });
 
-app.post("/register",(req,res)=>{
-	var {name,username,email,password,leader,member} = req.body;
-	var user = {name:name,username:username,email:email,password:password,leader:leader,member:member,wing:"null"};
-	Users.create(user,(err,obj)=>{
-		if(err){
+app.post("/register", (req, res) => {
+	var { name, username, email, password, leader, member } = req.body;
+	var user = { name: name, username: username, email: email, password: password, leader: leader, member: member, wing: "null" };
+	Users.create(user, (err, obj) => {
+		if (err) {
 			console.log(err);
-			res.send({status:false, error:err})
+			res.send({ status: false, error: err })
 		}
-		else{
+		else {
 			res.send(obj);
 		}
 	});
 });
 
-app.post("/getRole",(req,res)=>{
-	var {username} = req.body;
-	Users.findOne({username:username},(err,obj)=>{
-		if(err){
+app.post("/getRole", (req, res) => {
+	var { username } = req.body;
+	Users.findOne({ username: username }, (err, obj) => {
+		if (err) {
 			console.log(err);
 			res.status(404).json(err);
 		}
@@ -103,173 +102,184 @@ app.post("/getRole",(req,res)=>{
 	});
 });
 
-app.post("/setLeader",(req,res)=>{
-	var {username} = req.body;
-	console.log('username is',username);
-	Users.findOneAndUpdate({username:username},{$set:{gname:username}},{new:true},(err,obj)=>{
-		if(err)
+app.post("/setLeader", (req, res) => {
+	var { username } = req.body;
+	console.log('username is', username);
+	Users.findOneAndUpdate({ username: username }, { $set: { gname: username } }, { new: true }, (err, obj) => {
+		if (err)
 			res.status(404).json(err);
-		else
-		{
+		else {
 			res.json(obj);
-			Users.findOneAndUpdate({username:username},{$set:{leader:true}},{new:true},(err,obj)=>{
-				if(err)
+			Users.findOneAndUpdate({ username: username }, { $set: { leader: true } }, { new: true }, (err, obj) => {
+				if (err)
 					console.log(err);
 				else
 					console.log(obj);
 			});
 		}
-});	
+	});
 });
 
-app.post("/setMember",(req,res)=>{
-	var {username} = req.body;
-	console.log('username is',username);
-	Users.findOneAndUpdate({username:username},{$set:{member:true}},{new:true},(err,obj)=>{
-		if(err)
+app.post("/setMember", (req, res) => {
+	var { username } = req.body;
+	console.log('username is', username);
+	Users.findOneAndUpdate({ username: username }, { $set: { member: true } }, { new: true }, (err, obj) => {
+		if (err)
 			res.status(404).json(err);
 		else
 			res.json(obj);
-		});	
+	});
 });
 
 
-app.post("/createNotif",(req,res)=>{
-	var {fromGname,toUsername,fromUsername,toGname} = req.body;
+app.post("/createNotif", (req, res) => {
+	var { fromGname, toUsername, fromUsername, toGname } = req.body;
 	// console.log('username is',username);
-	var notif = {fromgname:fromGname,tousername:toUsername,fromusername:fromUsername,togname:toGname}
-	Notification.create(notif,(err,obj)=>{
-		if(err)
-			res.status(404).json(err);
-		else
-			res.json(obj);
-		});	
-});
-
-app.post("/getNotifsformember",(req,res)=>{
-	var {username} = req.body;
-	// console.log('username is',username);
-	Notification.find({tousername:username},(err,obj)=>{
+	var notif = { fromgname: fromGname, tousername: toUsername, fromusername: fromUsername, togname: toGname }
+	Notification.find(notif,(err,obj)=>{
 		if(err)
 			res.status(404).json(err);
 		else{
+			if(obj.length===0){
+				console.log('yes');
+				Notification.create(notif, (err, obj) => {
+					if (err)
+						res.status(404).json(err);
+					else
+						res.json(obj);
+				})
+			}
+			else{
+				res.status(200).json({
+					code: 123,
+					message: 'Notif already exists',
+				})
+			}
+		}
+	})
+});
+
+app.post("/getNotifsformember", (req, res) => {
+	var { username } = req.body;
+	// console.log('username is',username);
+	Notification.find({ tousername: username }, (err, obj) => {
+		if (err)
+			res.status(404).json(err);
+		else {
 			console.log(obj)
 			res.json(obj);
 		}
-		});	
+	});
 });
 
-app.post("/getNotifsforLeader",(req,res)=>{
-	var {username} = req.body;
+app.post("/getNotifsforLeader", (req, res) => {
+	var { username } = req.body;
 	// console.log('username is',username);
-	Notification.find({togname:username},(err,obj)=>{
-		if(err)
+	Notification.find({ togname: username }, (err, obj) => {
+		if (err)
 			res.status(404).json(err);
 		else
 			res.json(obj);
-		});	
+	});
 });
 
-app.post("/getUsers",(req,res)=>{
-	var {leader} = req.body;
-	Users.find({leader:leader},(err,obj)=>{
-		if(err)
+app.post("/getUsers", (req, res) => {
+	var { leader } = req.body;
+	Users.find({ leader: leader }, (err, obj) => {
+		if (err)
 			res.status(404).json(err);
 		else
 			res.json(obj);
-		});	
+	});
 });
 
-app.post("/getGroups",(req,res)=>{
-	var {gname,grpid,members,color} = req.body;
-	var grp={gname:gname,grpid:grpid,members:members,color:color};
-	GroupList.create(grp,(err,obj)=>{
-		if(err)
+app.post("/getGroups", (req, res) => {
+	var { gname, grpid, members, color } = req.body;
+	var grp = { gname: gname, grpid: grpid, members: members, color: color };
+	GroupList.create(grp, (err, obj) => {
+		if (err)
 			res.status(404).json(err);
 		else
 			res.json(obj);
-		});	
+	});
 });
 
-app.post("/getOneUser",(req,res)=>{
-	var {username} =req.body;
-	Users.find({username:username},(err,obj)=>{
-		if(err)
+app.post("/getOneUser", (req, res) => {
+	var { username } = req.body;
+	Users.find({ username: username }, (err, obj) => {
+		if (err)
 			res.status(404).json(err);
 		else
 			res.json(obj);
-		});	
+	});
 });
 
-app.post("/setSelected",(req,res)=>{
-	var {bhawan,floor,wingNo}=req.body;
-	console.log(bhawan,wingNo,floor);
-	Wing.findOneAndUpdate({bhawan:bhawan,floor:floor,wingNo:wingNo},{$set:{status:"selected"}},{new:true},(err,obj)=>{
+app.post("/setSelected", (req, res) => {
+	var { bhawan, floor, wingNo } = req.body;
+	console.log(bhawan, wingNo, floor);
+	Wing.findOneAndUpdate({ bhawan: bhawan, floor: floor, wingNo: wingNo }, { $set: { status: "selected" } }, { new: true }, (err, obj) => {
 		console.log(obj);
-		if(err)
+		if (err)
 			res.status(404).json(err);
 		else
 			res.json(obj);
 	})
 })
 
-app.post("/selectWing",(req,res)=>{
-	var {wing,user}=req.body;
-	console.log("hiii"+wing+user);
-	Users.findOneAndUpdate({username:user},{$set:{wing:wing}},(err,obj)=>{
+app.post("/selectWing", (req, res) => {
+	var { wing, user } = req.body;
+	console.log("hiii" + wing + user);
+	Users.findOneAndUpdate({ username: user }, { $set: { wing: wing } }, (err, obj) => {
 		console.log(obj);
-		if(err)
+		if (err)
 			res.status(404).json(err);
 		else
 			res.json(obj);
 	})
 })
 
-app.post("/setBlocked",(req,res)=>{
-	var {bhawan,floor,wingNo}=req.body;
-	Wing.findOneAndUpdate({bhawan:bhawan,floor:floor,wingNo:wingNo},{$set:{status:"blocked"}},{new:true},(err,obj)=>{
+app.post("/setBlocked", (req, res) => {
+	var { bhawan, floor, wingNo } = req.body;
+	Wing.findOneAndUpdate({ bhawan: bhawan, floor: floor, wingNo: wingNo }, { $set: { status: "blocked" } }, { new: true }, (err, obj) => {
 		console.log(obj);
-		if(err)
+		if (err)
 			res.status(404).json(err);
 		else
 			res.json(obj);
 	})
 })
 
-app.post("/setFree",(req,res)=>{
-	var {bhawan,floor,wingNo}=req.body;
-	Wing.findOneAndUpdate({bhawan:bhawan,floor:floor,wingNo:wingNo},{$set:{status:"free"}},{new:true},(err,obj)=>{
+app.post("/setFree", (req, res) => {
+	var { bhawan, floor, wingNo } = req.body;
+	Wing.findOneAndUpdate({ bhawan: bhawan, floor: floor, wingNo: wingNo }, { $set: { status: "free" } }, { new: true }, (err, obj) => {
 		console.log(obj);
-		if(err)
+		if (err)
 			res.status(404).json(err);
 		else
 			res.json(obj);
 	})
 })
 
-app.post("/notifsReq",(req,res)=>{
-	var {fromGname,toUsername,fromUsername,toGname,accept} = req.body;
-	console.log('fromGname',fromGname)
-	if(fromGname!==undefined && toUsername!==undefined)
-	{
-		if(accept===true)
-		{
-			Notification.findOneAndUpdate({fromgname:fromGname,tousername:toUsername},{$set:{colour:"#00ff00",disabled:true}},{new:true},(err,obj)=>{
+app.post("/notifsReq", (req, res) => {
+	var { fromGname, toUsername, fromUsername, toGname, accept } = req.body;
+	console.log('fromGname', fromGname)
+	if (fromGname !== undefined && toUsername !== undefined) {
+		if (accept === true) {
+			Notification.findOneAndUpdate({ fromgname: fromGname, tousername: toUsername }, { $set: { colour: "#00ff00", disabled: true } }, { new: true }, (err, obj) => {
 				console.log(obj);
 			})
-			Users.findOneAndUpdate({username:toUsername},{$set:{gname:fromGname}},{new:true},(err,obj)=>{
+			Users.findOneAndUpdate({ username: toUsername }, { $set: { gname: fromGname } }, { new: true }, (err, obj) => {
 				console.log(obj);
-				if(err)
+				if (err)
 					res.status(404).json(err);
 				else
 					res.json(obj);
 			})
 		}
-		else
-		{
-			Notification.findOneAndUpdate({fromgname:fromGname,tousername:toUsername},{$set:{colour:"#dc143c",disabled:true}},{new:true},(err,obj)=>{
+		else {
+			Notification.findOneAndUpdate({ fromgname: fromGname, tousername: toUsername }, { $set: { colour: "#dc143c", disabled: true } }, { new: true }, (err, obj) => {
 				console.log(obj);
-				if(err)
+				if (err)
 					res.status(404).json(err);
 				else
 					res.json(obj);
@@ -277,26 +287,23 @@ app.post("/notifsReq",(req,res)=>{
 		}
 	}
 	else
-		if(fromUsername!==undefined && toGname!==undefined)
-		{
-			if(accept===true)
-			{
-				Notification.findOneAndUpdate({fromusername:fromUsername,togname:toGname},{$set:{colour:"#00ff00",disabled:true}},{new:true},(err,obj)=>{
+		if (fromUsername !== undefined && toGname !== undefined) {
+			if (accept === true) {
+				Notification.findOneAndUpdate({ fromusername: fromUsername, togname: toGname }, { $set: { colour: "#00ff00", disabled: true } }, { new: true }, (err, obj) => {
 					console.log(obj);
 				})
-				Users.findOneAndUpdate({username:fromUsername},{$set:{gname:toGname}},{new:true},(err,obj)=>{
+				Users.findOneAndUpdate({ username: fromUsername }, { $set: { gname: toGname } }, { new: true }, (err, obj) => {
 					console.log(obj);
-					if(err)
+					if (err)
 						res.status(404).json(err);
 					else
 						res.json(obj);
 				})
 			}
-			else
-			{
-				Notification.findOneAndUpdate({fromusername:fromUsername,togname:toGname},{$set:{colour:"#dc143c",disabled:true}},{new:true},(err,obj)=>{
+			else {
+				Notification.findOneAndUpdate({ fromusername: fromUsername, togname: toGname }, { $set: { colour: "#dc143c", disabled: true } }, { new: true }, (err, obj) => {
 					console.log(obj);
-					if(err)
+					if (err)
 						res.status(404).json(err);
 					else
 						res.json(obj);
@@ -305,34 +312,34 @@ app.post("/notifsReq",(req,res)=>{
 		}
 })
 
-app.post('/timer',(req,res)=>{
-	var {timer} = req.body;
+app.post('/timer', (req, res) => {
+	var { timer } = req.body;
 	var time = {
 		countdown: new Date(),
-		updatedAt:new Date(),
+		updatedAt: new Date(),
 	}
-	Timer.create(time,(err,obj)=>{
-		if(err)
+	Timer.create(time, (err, obj) => {
+		if (err)
 			res.status(500).json(err);
 		else
-			res.status(201).json({id:obj.id});	
+			res.status(201).json({ id: obj.id });
 	})
 })
 
-app.get('/timer/:id',(req,res)=>{
+app.get('/timer/:id', (req, res) => {
 	var id = req.params.id;
-	Timer.findById(id,(err,obj)=>{
-		if(err)
+	Timer.findById(id, (err, obj) => {
+		if (err)
 			res.status(500).json(err);
 		else
 			res.status(200).json(obj);
 	})
 })
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
 	res.status(200).json('Server is up and running')
 })
 
-app.listen(process.env.PORT || 5000,()=>{
+app.listen(process.env.PORT || 5000, () => {
 	console.log(`listening on port ${process.env.PORT}`);
 });
