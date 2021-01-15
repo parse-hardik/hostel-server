4a4ba01020f07105931b37b03aceed55f7c9a5c2
parse-hardik <f20180062@hyderabad.bits-pilani.ac.in>
2021-01-15 17:24:06 +0530

    Deleting config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	mongooseConnection: "mongodb+srv://hardik:hardik@cluster0-zs92y.mongodb.net/test?retryWrites=true&w=majority",
-	GOOGLE_CLIENT_ID: "345909468356-4vbjp80iocrhij7ndm8p5pr56vrl5c32.apps.googleusercontent.com",
-	GOOGLE_CLIENT_SECRET: "lH18J0A7l-QW4QKlDRKZ5c6Z",
-};
